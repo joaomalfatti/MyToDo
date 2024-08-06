df1e02ac073bed048861c46ca73d7e26d558a0fa
joaomalfatti <joao.cursos1210@gmail.com>
2024-08-05 21:59:51 -0400

    Foi criado STYLE GLOBAL do APP.

---
+++ src/theme/index.ts
@@ -0,0 +1,31 @@
+export default {
+  COLORS:{
+
+    /* Produto*/
+    BLUE_DARK: '#1E6F9F',
+    BLUE: '#4EA8DE',
+    PURPLE_DARK: '#5E60CE',
+    PURPLE: '#8284FA',
+
+    /*BASE*/
+    GRAY_700: '#0D0D0D',
+    GRAY_600: '#1A1A1A',
+    GRAY_500: '#262626',
+    GRAY_400: '#333333',
+    GRAY_300: '#808080',
+    GRAY_200: '#D9D9D9',
+    GRAY_100: '#F2F2F2',
+
+    /*FEEDBACK*/
+    DANGER: '#E25858'
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Inter_400Regular',
+    BOLD: 'Inter_700Bold',
+  },
+  FONT_SIZE:{
+    SM: 12,
+    MD: 14,
+    LG: 16,
+  }
+}
